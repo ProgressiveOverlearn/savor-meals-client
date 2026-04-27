4d2b7a1b2541457f440c79508bb048aa7981b8a7
ProgressiveOverlearn <miguelmelendez814@gmail.com>
2026-04-27 01:10:18 -0400

    React front-end of Savor Meals

--- src/shared/header.jsx
+++ src/shared/header.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
 function Header({ user }) {
+
+//hamburger menu icon for smaller screens
+const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 return (
     <header>
         <nav className="navbar has-background-success-40 has-shadow">
             <div className="navbar-brand p-3 m-1">
-                <img src="images/SAVOR MEALS.png" alt="website logo" width="80" height="auto"/>
+                <img src="images/SAVOR MEALS.png" alt="website logo" width="80" height="auto" />
+                <a role="button" className="navbar-burger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    <i className={`fa-solid ${isMenuOpen ? 'fa-xmark' : 'fa-bars'}`}></i>
+                </a>
+
             </div>
-            <div className="navbar-menu" id="nav-links">
+
+            {/* div has the navbar-menu classname no matter what, but gets additional classes if the burger icon shows up, causing the navigation links to show up after clicking the burger icon */}
+            <div className={`navbar-menu ${isMenuOpen ? 'is-active has-background-success-40 has-shadow' : ''}`} id="nav-links">
                 <div className="navbar-end">
                     <a className="navbar-item has-text-white" href="/"><i className="fa-solid fa-house"></i>HOME</a>
                     <a className="navbar-item has-text-white" href="/find"><i className="fa-solid fa-magnifying-glass"></i>FIND</a>
